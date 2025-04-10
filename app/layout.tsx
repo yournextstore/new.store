b5172d451439c5b1fcb57d58506213e6c4782495
Grzegorz Kossakowski <grzegorz.kossakowski@gmail.com>
2025-04-10 11:46:29 +0200

    Title and description (html) for new.store

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,8 +7,8 @@ import './globals.css';
 
 export const metadata: Metadata = {
   metadataBase: new URL('https://chat.vercel.ai'),
-  title: 'Next.js Chatbot Template',
-  description: 'Next.js chatbot template using the AI SDK.',
+  title: 'new.store',
+  description: 'new.store is a tool for generating storefronts for YourNextStore using AI.',
 };
 
 export const viewport = {
