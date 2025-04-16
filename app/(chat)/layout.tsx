4be367d56331bff09e79f13d018205dc099d9e68
Michal Miszczyszyn <michal@mmiszy.pl>
2025-04-16 12:09:03 +0200

    cleanup 2

--- app/(chat)/layout.tsx
+++ app/(chat)/layout.tsx
@@ -1,7 +1,4 @@
 
-import { SidebarInset, SidebarProvider } from '@/components/ui/sidebar';
-import Script from 'next/script';
-
 export const experimental_ppr = true;
 
 export default async function Layout({
@@ -9,19 +6,5 @@ export default async function Layout({
 }: {
   children: React.ReactNode;
 }) {
-  // const [session, cookieStore] = await Promise.all([auth(), cookies()]);
-  // const isCollapsed = cookieStore.get('sidebar:state')?.value !== 'true';
-
-  return (
-    <>
-      <Script
-        src="https://cdn.jsdelivr.net/pyodide/v0.23.4/full/pyodide.js"
-        strategy="beforeInteractive"
-      />
-      <SidebarProvider defaultOpen={true}>
-        {/* <AppSidebar user={session?.user} /> */}
-        <SidebarInset>{children}</SidebarInset>
-      </SidebarProvider>
-    </>
-  );
+  return (children);
 }
