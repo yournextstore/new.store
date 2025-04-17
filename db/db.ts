e95e1f5f1bcc23332150f06b7e10c8fc0b72b723
Michal Miszczyszyn <michal@mmiszy.pl>
2025-04-17 09:53:24 +0200

    fix drizzle config 2

--- db/db.ts
+++ db/db.ts
@@ -1,29 +1,37 @@
-import { invariant } from "@/lib/utils";
-import { Pool, neonConfig } from "@neondatabase/serverless";
-import { loadEnvConfig } from "@next/env";
-import type { DBQueryConfig, ExtractTablesWithRelations } from "drizzle-orm";
-import { type NeonQueryResultHKT, drizzle } from "drizzle-orm/neon-serverless";
-import type { PgTransaction } from "drizzle-orm/pg-core";
-import ws from "ws";
-import * as schema from "./schema";
+import { invariant } from '@/lib/utils';
+import { Pool, neonConfig } from '@neondatabase/serverless';
+import { loadEnvConfig } from '@next/env';
+import type { DBQueryConfig, ExtractTablesWithRelations } from 'drizzle-orm';
+import { type NeonQueryResultHKT, drizzle } from 'drizzle-orm/neon-serverless';
+import type { PgTransaction } from 'drizzle-orm/pg-core';
+import ws from 'ws';
+import * as schema from './schema';
 
-loadEnvConfig(".");
+loadEnvConfig('.');
 neonConfig.webSocketConstructor = ws;
 
-invariant(process.env.DATABASE_URL, "DATABASE_URL is required");
+invariant(process.env.DATABASE_URL, 'DATABASE_URL is required');
 export const pool = new Pool({ connectionString: process.env.DATABASE_URL });
-export const db = drizzle(pool, { schema, logger: false });
+export const db = drizzle(pool, {
+  schema,
+  logger: false,
+  casing: 'snake_case',
+});
 export { schema };
 
 export type DrizzleClient = typeof db;
 
 export default db;
 
-export type Trx = PgTransaction<NeonQueryResultHKT, typeof schema, ExtractTablesWithRelations<typeof schema>>;
+export type Trx = PgTransaction<
+  NeonQueryResultHKT,
+  typeof schema,
+  ExtractTablesWithRelations<typeof schema>
+>;
 export type DbWith<TableName extends keyof TSchema> = DBQueryConfig<
-	"one" | "many",
-	boolean,
-	TSchema,
-	TSchema[TableName]
->["with"];
+  'one' | 'many',
+  boolean,
+  TSchema,
+  TSchema[TableName]
+>['with'];
 type TSchema = ExtractTablesWithRelations<typeof schema>;
