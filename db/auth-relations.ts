e041e9e8171495b4c3f4e5bb69bc99468461357d
Jakub Neander <oh@zaiste.net>
2025-04-17 09:07:22 +0200

    refactor: remove schema

--- db/auth-relations.ts
+++ db/auth-relations.ts
@@ -1,6 +1,12 @@
 import { relations } from "drizzle-orm";
-import { accounts, invitations, members, organizations, sessions, users2s } from "./auth-schema";
-import { stores } from "./base-schema";
+import {
+	accounts,
+	invitations,
+	members,
+	organizations,
+	sessions,
+	users2s,
+} from "./auth-schema";
 
 export const users2Relations = relations(users2s, ({ many }) => ({
 	sessions: many(sessions),
@@ -14,16 +20,27 @@ export const sessionRelations = relations(sessions, ({ one }) => ({
 export const accountsRelations = relations(accounts, ({ one }) => ({
 	user: one(users2s, { references: [users2s.id], fields: [accounts.userId] }),
 }));
-export const organizationsRelations = relations(organizations, ({ many, one }) => ({
-	members: many(members),
-	invitations: many(invitations),
-	store: one(stores),
-}));
+export const organizationsRelations = relations(
+	organizations,
+	({ many, one }) => ({
+		members: many(members),
+		invitations: many(invitations),
+	}),
+);
 export const membersRelations = relations(members, ({ one }) => ({
-	organization: one(organizations, { references: [organizations.id], fields: [members.organizationId] }),
+	organization: one(organizations, {
+		references: [organizations.id],
+		fields: [members.organizationId],
+	}),
 	user: one(users2s, { references: [users2s.id], fields: [members.userId] }),
 }));
 export const invitationsRelations = relations(invitations, ({ one }) => ({
-	organizations: one(organizations, { references: [organizations.id], fields: [invitations.organizationId] }),
-	inviter: one(users2s, { references: [users2s.id], fields: [invitations.inviterId] }),
+	organizations: one(organizations, {
+		references: [organizations.id],
+		fields: [invitations.organizationId],
+	}),
+	inviter: one(users2s, {
+		references: [users2s.id],
+		fields: [invitations.inviterId],
+	}),
 }));
--- db/schema.ts
+++ db/schema.ts
@@ -1,3 +1,2 @@
 export * from "./auth-relations";
 export * from "./auth-schema";
-export * from "./base-schema";
