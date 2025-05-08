5e3a9fe944d21efff915bc52b5fcf5eac455a9d9
Grzegorz Kossakowski <grzegorz.kossakowski@gmail.com>
2025-05-08 20:31:18 +0200

    Remove dead code

--- app/chat.tsx
+++ app/chat.tsx
@@ -6,7 +6,6 @@ import { RadioGroup, RadioGroupItem } from '@/components/ui/radio-group';
 import { Label } from '@/components/ui/label';
 import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
 import type { User } from 'better-auth';
-import { CheckIcon, ClipboardIcon } from 'lucide-react';
 import { useEffect, useState } from 'react';
 import SyntaxHighlighter from 'react-syntax-highlighter';
 import { okaidia } from 'react-syntax-highlighter/dist/esm/styles/prism';
